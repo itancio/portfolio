4a4a425e71b499e861237b378562bf0a02c4008c
Irvin Tancioco <irvin.tanc@gmail.com>
2024-07-25 23:04:55 -0700

    Add.storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,17 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/nextjs',
-    options: {}
+    options: {},
   },
-  docs: {
-    autodocs: 'tag'
-  }
+  staticDirs: ['../public'],
 };
 export default config;
